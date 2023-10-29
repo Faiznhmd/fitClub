c1e10227f6c752e437e3376d16c5604fae7f8204
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-10-29 23:58:32 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Hero from './components/Hero.jsx';
-import Program from './components/Program.jsx';
+import Plans from './components/Plans';
+import Program from './components/Program';
+import Reason from './components/Reason';
+import Testimonials from './components/Testimonials';
+import Join from './components/Join';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Program />
+      <Reason />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,82 @@
 import Logo from '../assets/logo.png';
 import './header.css';
+import Bar from '../assets/bars.png';
+import { useState } from 'react';
+import { Link } from 'react-scroll';
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
-    <div className="header">
+    <div className="header" id="home">
       <img src={Logo} alt="" className="logo" />
-      <ul className="header__menu">
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why Us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            background: 'gray',
+            padding: '0.5rem',
+            borderRadius: '5px',
+            cursor: 'pointer',
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img src={Bar} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+        </div>
+      ) : (
+        <ul className="header__menu">
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to="header"
+              spy={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="Programs"
+              spy={true}
+              smooth={true}
+            >
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="Why Us"
+              spy={true}
+              smooth={true}
+            >
+              {' '}
+              Why Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="Plans"
+              spy={true}
+              smooth={true}
+            >
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="Testimonials"
+              spy={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
